60811385b7cf2bec80c3540da7a405bf1578b7a2
Andrei Galitianu <andrei.galitianu@ciconia-systems.com>
2024-04-06 22:12:24 +0300

    finished implementation

--- DES.java
+++ DES.java
@@ -23,6 +23,65 @@ public class DES {
     static int[] PC2 = { 14, 17, 11, 24, 1, 5, 3, 28, 15, 6, 21, 10, 23, 19, 12, 4, 26, 8, 16, 7, 27, 20, 13, 2, 41, 52,
             31, 37, 47, 55, 30, 40, 51, 45, 33, 48, 44, 49, 39, 56, 34, 53, 46, 42, 50, 36, 29, 32 };
 
+    static int[] E = { 32, 1, 2, 3, 4, 5, 4, 5, 6, 7, 8, 9, 8, 9, 10, 11, 12, 13, 12, 13, 14, 15, 16, 17, 16, 17, 18,
+            19, 20, 21, 20, 21, 22, 23, 24, 25, 24, 25, 26, 27, 28, 29, 28, 29, 30, 31, 32, 1 };
+
+    static int[] P = { 16, 7, 20, 21, 29, 12, 28, 17, 1, 15, 23, 26, 5, 18, 31, 10, 2, 8, 24, 14, 32, 27, 3, 9, 19, 13,
+            30, 6, 22, 11, 4, 25 };
+
+    static int[] IP_1 = { 40, 8, 48, 16, 56, 24, 64, 32, 39, 7, 47, 15, 55, 23, 63, 31, 38, 6, 46, 14, 54, 22, 62, 30,
+            37, 5, 45, 13, 53, 21, 61, 29, 36, 4, 44, 12, 52, 20, 60, 28, 35, 3, 43, 11, 51, 19, 59, 27, 34, 2, 42, 10,
+            50, 18, 58, 26, 33, 1, 41, 9, 49, 17, 57, 25 };
+
+    static int[][][] S = { { // S1
+            { 14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7 },
+            { 0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8 },
+            { 4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0 },
+            { 15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13 }
+    },
+            { // S2
+                    { 15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10 },
+                    { 3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5 },
+                    { 0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15 },
+                    { 13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9 }
+            },
+            { // S3
+                    { 10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8 },
+                    { 13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1 },
+                    { 13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7 },
+                    { 1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12 }
+            },
+            { // S4
+                    { 7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15 },
+                    { 13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9 },
+                    { 10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4 },
+                    { 3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14 }
+            },
+            { // S5
+                    { 2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9 },
+                    { 14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6 },
+                    { 4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14 },
+                    { 11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3 }
+            },
+            { // S6
+                    { 12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11 },
+                    { 10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8 },
+                    { 9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6 },
+                    { 4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13 }
+            },
+            { // S7
+                    { 4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1 },
+                    { 13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6 },
+                    { 1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2 },
+                    { 6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12 }
+            },
+            { // S8 finally
+                    { 13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7 },
+                    { 1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2 },
+                    { 7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8 },
+                    { 2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11 }
+            } };
+
     private static long map(long input, int[] map) {
         long result = 0;
         for (int i = 0; i < map.length; i++) {
@@ -36,54 +95,57 @@ public class DES {
         return map(plaintext, IP);
     }
 
+    private static long finalPermutation(long text) {
+        return map(text, IP_1);
+    }
+
     private static Pair<Long, Long> keyExpansion(long key, int shift) {
         long compressed = map(key, PC1);
         long h1 = Long.rotateLeft(compressed >>> 28, shift);
         long h2 = Long.rotateLeft(compressed & 0xfffffff, shift);
-        System.out.println("Key" + Long.toHexString(h1));
-        System.out.println("Key" + Long.toHexString(h2));
+        // System.out.println("Key" + Long.toHexString(h1));
+        // System.out.println("Key" + Long.toHexString(h2));
 
         long subKey = map((h1 << 28) | h2, PC2);
-        System.out.println(Long.toHexString(subKey));
+        // System.out.println(Long.toHexString(subKey));
         long nextKey = (h1 << 28) | h2;
         return new Pair<>(subKey, nextKey);
     }
 
-    // static long removeBit(long x, int i) {
-    // long mask = -1 << i;
-    // return (x & ~mask) | ((x >>> 1) & mask);
-    // }
+    private static long expansionPermutation(long text, long subKey, int[][] sKey) {
+        long left = text >>> 32;
+        long right = text & 0xffffffff;
+        long newRight = map(right, E) ^ subKey;
 
-    // private static long keyTransformation(long key, int shift) {
-    // System.out.println("Key " + Long.toHexString(key));
+        long mask = 0x3F;
+        long newnewright = 0;
+        for (int i = 0; i < 8; i++) {
+            long mini = newRight & mask;
+            long inner = mini & 0x1E;
+            long outer = (mini & 1) | ((mini & 0x20) >>> 4);
+            long rez = sKey[(int) inner][(int) outer];
+            newnewright = newnewright | (rez << (i * 4));
+            mask = mask << 6;
+        }
 
-    // for (int i = 7; i < 64; i += 7) {
-    // key = removeBit(key, i);
-    // }
-    // System.out.println("Key-8 " + Long.toHexString(key));
-
-    // long h1 = key >>> 28;
-    // long h2 = key & 0xfffffff;
-
-    // System.out.println("H1 " + Long.toHexString(h1));
-    // System.out.println("H2 " + Long.toHexString(h2));
-
-    // return key;
-    // }
+        newnewright = map(newnewright, P) ^ left;
+        return (left << 32) | newnewright;
+    }
 
     public static void main(String[] args) {
         long plaintext = Long.parseUnsignedLong("8F03456D3F78E2C5", 16);
         long key = Long.parseUnsignedLong("3b3898371520f75e", 16);
 
-        initialPermutation(plaintext);
-        System.out.println(Long.toHexString(map(key, PC1)));
-        keyExpansion(key, 1);
+        long text = initialPermutation(plaintext);
+        for (int i = 0; i < leftShifts.length; i++) {
+            Pair<Long, Long> keyOutput = keyExpansion(key, leftShifts[i]);
+
+            text = expansionPermutation(text, keyOutput.first, S[i]);
+            key = keyOutput.second;
+        }
+
+        text = finalPermutation(text);
+
+        System.out.println(Long.toHexString(text));
     }
 }
-
-// 0011101100111000100110000011011100010101001000001111011101011110
-// 0111011 0111000 0011000 0110111 0010101 0100000 1110111 1011110
-// 0111011 0111000 0011000 0110111 0010101 0100000 1110111 1011110
-// 8df6bc07d8c959
-// 1111101100011001001010110010
-// 0111110110001100100101011001
